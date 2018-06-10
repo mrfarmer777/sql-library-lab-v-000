0c88d74ca2c90ae11326a7a358184effce04882d
Matt Farmer <mrfarmer777@gmail.com>
2018-06-10 02:55:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year, FROM books WHERE series_id=1 ORDER BY year"
+  "SELECT title, year FROM books WHERE series_id=1 ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.title) FROM characters INNER JOIN "
 end
