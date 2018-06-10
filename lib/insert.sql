1cb70e63a1c08faf32fbceb30c54c479d3d65d2a
Matt Farmer <mrfarmer777@gmail.com>
2018-06-10 02:14:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,11 @@
 INSERT INTO series (title,author_id,subgenre_id) VALUES
-("Lord of the Rings",1,1)
+("Lord of the Rings",1,1),
 ("Fjord of the Blings",2,1);
 
 INSERT INTO subgenres(name) VALUES
-("Classical Fantasy")
-("Norwegian Fantasy Parody")
+("Classical Fantasy"),
+("Norwegian Fantasy Parody");
+
+INSERT INTO authors VALUES
+("J.R.R. Tolkien"),
+("M.L.L. Fjurstenborg");
