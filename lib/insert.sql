e343804a6321de5bde0a8eec01f4de6f24035095
Matt Farmer <mrfarmer777@gmail.com>
2018-06-10 02:20:04 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,6 +6,15 @@ INSERT INTO subgenres(name) VALUES
 ("Classical Fantasy"),
 ("Norwegian Fantasy Parody");
 
-INSERT INTO authors VALUES
+INSERT INTO authors(name) VALUES
 ("J.R.R. Tolkien"),
 ("M.L.L. Fjurstenborg");
+
+INSERT INTO books(title, year, series_id) VALUES 
+("The Fellowship of the Ring",1942,1),
+("The Two Towers",1944,1),
+("The Return of the King",1946,1),
+("Der Splurgen dur the Frung",1965,2),
+("Illiemsturgen!", 1966,2),
+("The Phantom Menace",1967,2);
+
